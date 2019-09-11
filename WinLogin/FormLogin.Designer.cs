dead93402393a74efb7f674ddf86bfd2664b91bb
cu3l3bre <cu3l3bre@googlemail.com>
2019-09-11 15:12:39 +0200

    Projektdateien hinzufügen.

---
+++ WinLogin/FormLogin.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WinLogin
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.labelPasswort = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxPasswort = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonRegistrieren = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelLoggedUser = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new System.Drawing.Point(129, 175);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(119, 23);
+            this.buttonLogout.TabIndex = 11;
+            this.buttonLogout.Text = "&Ausloggen";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.ButtonLogout_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(129, 139);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(119, 23);
+            this.buttonLogin.TabIndex = 10;
+            this.buttonLogin.Text = "&Einloggen";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.ButtonLogin_Click);
+            // 
+            // labelPasswort
+            // 
+            this.labelPasswort.AutoSize = true;
+            this.labelPasswort.Location = new System.Drawing.Point(30, 71);
+            this.labelPasswort.Name = "labelPasswort";
+            this.labelPasswort.Size = new System.Drawing.Size(53, 13);
+            this.labelPasswort.TabIndex = 9;
+            this.labelPasswort.Text = "Passwort:";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(27, 30);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(58, 13);
+            this.labelUsername.TabIndex = 8;
+            this.labelUsername.Text = "Username:";
+            // 
+            // textBoxPasswort
+            // 
+            this.textBoxPasswort.Location = new System.Drawing.Point(89, 71);
+            this.textBoxPasswort.Name = "textBoxPasswort";
+            this.textBoxPasswort.Size = new System.Drawing.Size(159, 20);
+            this.textBoxPasswort.TabIndex = 7;
+            this.textBoxPasswort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPasswort_KeyDown);
+            this.textBoxPasswort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPasswort_KeyPress);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(89, 30);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(159, 20);
+            this.textBoxUsername.TabIndex = 6;
+            // 
+            // buttonRegistrieren
+            // 
+            this.buttonRegistrieren.Location = new System.Drawing.Point(129, 105);
+            this.buttonRegistrieren.Name = "buttonRegistrieren";
+            this.buttonRegistrieren.Size = new System.Drawing.Size(119, 23);
+            this.buttonRegistrieren.TabIndex = 12;
+            this.buttonRegistrieren.Text = "&Registrieren";
+            this.buttonRegistrieren.UseVisualStyleBackColor = true;
+            this.buttonRegistrieren.Click += new System.EventHandler(this.ButtonRegistrieren_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(30, 206);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(137, 13);
+            this.labelStatus.TabIndex = 13;
+            this.labelStatus.Text = "Anmelden oder Registrieren";
+            // 
+            // labelLoggedUser
+            // 
+            this.labelLoggedUser.AutoSize = true;
+            this.labelLoggedUser.Location = new System.Drawing.Point(126, 206);
+            this.labelLoggedUser.Name = "labelLoggedUser";
+            this.labelLoggedUser.Size = new System.Drawing.Size(0, 13);
+            this.labelLoggedUser.TabIndex = 14;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 231);
+            this.Controls.Add(this.labelLoggedUser);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonRegistrieren);
+            this.Controls.Add(this.buttonLogout);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPasswort);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPasswort);
+            this.Controls.Add(this.textBoxUsername);
+            this.Name = "FormLogin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label labelPasswort;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxPasswort;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonRegistrieren;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelLoggedUser;
+    }
+}
+
---
+++ WinLogin/FormLogin.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace WinLogin
+{
+    // Übungsvorschlag: Login-Fenster
+    //
+    //      - Authentifizierung des Benutzer via Datei 'Daten\Login.txt'
+    //      - das vom Benutzer eingegebene Passwort darf nicht im "Klartext"
+    //        in der 'TextBox' erscheinen (Hinweis 'KeyPress'-Ereignis)
+    //      - Log-Datei, welche alle An- und Abmeldungen von Benutzers protokolliert
+    //
+
+
+    public partial class FormLogin : Form
+    {
+        private Dictionary<string, string> userPwList = new Dictionary<string, string>();
+        private static readonly string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..", "Daten", "Login.txt"));
+
+        private string password = String.Empty;
+
+
+        public FormLogin()
+        {
+            InitializeComponent();
+            readDataFromFile();
+        }
+
+
+
+
+
+        private bool readDataFromFile()
+        {
+            StreamReader readFromFile = null;
+
+            try
+            {
+
+                readFromFile = new StreamReader(path, Encoding.Default);
+
+                while (!readFromFile.EndOfStream)
+                {
+                    string line = readFromFile.ReadLine();
+                    string[] data = line.Split(';');
+                    userPwList.Add(data[0], data[1]);
+
+                    Debug.WriteLine(data[0] + " " + data[1]);
+                }
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("Something went wrong");
+                return false;
+            }
+            finally
+            {
+                if(readFromFile != null)
+                {
+                    readFromFile.Close();
+                }
+            }
+        }
+
+
+        private bool writeDataToFile()
+        {
+            // TODO
+            return true;
+        }
+
+
+        private void ButtonRegistrieren_Click(object sender, EventArgs e)
+        {
+            bool registerOK = false;
+            registerOK = register();
+
+            if(registerOK)
+            {
+                writeDataToFile();
+            }
+
+        }
+
+        private void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+
+        private void ButtonLogout_Click(object sender, EventArgs e)
+        {
+            logout();
+        }
+
+
+
+        private void login()
+        {
+
+            if (!(String.IsNullOrWhiteSpace(textBoxUsername.Text) || String.IsNullOrWhiteSpace(textBoxPasswort.Text)))
+            {
+
+                if (userPwList.TryGetValue(textBoxUsername.Text, out string pw))
+                {
+                    //if (pw.Equals(textBoxPasswort.Text))
+                    if (pw.Equals(password))
+                    {
+                        //Debug.WriteLine("Einloggen erfolgreich");
+                        buttonLogin.Enabled = false;
+                        buttonRegistrieren.Enabled = false;
+                        buttonLogout.Enabled = true;
+
+                        labelStatus.Text = "Angemeldet als " + textBoxUsername.Text;
+                        textBoxUsername.Text = String.Empty;
+                        textBoxPasswort.Text = String.Empty;
+
+                    }
+                    else
+                    {
+                        // Debug.WriteLine("Passwort falsch existiert nicht");
+                        MessageBox.Show("Eingegebene Daten ungueltig");
+                    }
+                }
+                else
+                {
+                    //Debug.WriteLine("User existiert nicht");
+                    MessageBox.Show("Einloggen fehlgeschlagen");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fuellen Sie alle Felder aus");
+            }
+        }
+
+
+
+        private void logout()
+        {
+            buttonLogin.Enabled = true;
+            buttonRegistrieren.Enabled = true;
+            buttonLogout.Enabled = false;
+
+            labelStatus.Text = "Anmelden oder Registrieren";
+            textBoxUsername.Text = String.Empty;
+            textBoxPasswort.Text = String.Empty;
+        }
+
+
+        private bool register()
+        {
+            if(!(String.IsNullOrWhiteSpace(textBoxUsername.Text) || String.IsNullOrWhiteSpace(textBoxPasswort.Text)))
+            {
+                if (!userPwList.ContainsKey(textBoxUsername.Text))
+                {
+                    userPwList.Add(textBoxUsername.Text, textBoxPasswort.Text);
+                    textBoxUsername.Text = String.Empty;
+                    textBoxPasswort.Text = String.Empty;
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Benutzername bereits vergeben");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nicht alle Felder ausgefuellt");
+                return false;
+            }
+        }
+
+
+
+        private void textBoxPasswort_KeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine("{0}", e.KeyValue);
+
+            if (e.KeyValue == 8 || e.KeyValue == 46 || e.KeyValue == 37 || e.KeyValue == 38 || e.KeyValue == 39 || e.KeyValue == 40)
+            {
+                textBoxPasswort.Text = String.Empty;
+                password = String.Empty;
+            }
+        }
+
+
+        private void textBoxPasswort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Debug.WriteLine("{0}", e.KeyChar);
+
+            if ((e.KeyChar >= (char)97) && (e.KeyChar <= (char)122)
+                || (e.KeyChar >= (char)65) && (e.KeyChar <= (char)90)
+                || (e.KeyChar >= (char)48) && (e.KeyChar <= (char)57)
+                )
+            {
+                password += e.KeyChar;
+                e.KeyChar = '*';
+                Debug.WriteLine(password);
+            }
+            else
+            {
+                e.KeyChar = (char)0;
+            }
+
+            e.Handled = false;
+        }
+
+
+
+
+
+    }
+}
